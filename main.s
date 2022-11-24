03b6754ffa660a07880f7bd81076cc4eee56b87e
Gal Pearl <galpearl24@gmail.com>
2022-11-24 16:28:43 +0200

    Part 2
    Finished part 2

--- main.s
+++ main.s
@@ -1,34 +1,40 @@
     .data
-id: .int 318778594
+id:  .int 318778594
 	.section 	.rodata
 format1: .ascii "%d\n"                        	          #id: 318778594
 	.text
 .globl	main	                           		#initial point of this program
     .type	main, @function                                    #the main label is a function
-    
-    
 main:
-    push 	%rbx					#start of main
+    movq   %rsp, %rbp #for correct debugging
+    push 	 %rbx					#start of main
     lea    format1(%rip), %rdi
-    mov    $318778594, %esi
+    mov    id, %esi
     call   printf
-    #part 2															#end of part 1
-    movq   $318778594, %rax
-    and    $1, %ah				#bitwise AND, and conditional jump
-    #jz     .even
+    #part 2
+    movq   id, %rax
+    and    $0x01, %ah				#bitwise AND, and conditional jump
+    jz     .even
     jmp    .odd
 
 .odd:       #If number is odd: mult by 3 and print
-####EVERYTHING WORKS####
-    mov    $318778594, %esi
+    mov    id, %esi
     leal   (%esi, %esi, 2), %esi
     lea    format1(%rip), %rdi
     call   printf
-        
+    jmp    .cont1
     
 .even:      #If number is even: remainder of div by 3
-    ####NEED TO MAKE####
-
+    and    $0x00, %rdx
+    and    $0x00, %rax
+    movq   id, %rbx                                        #insert ID
+    mov    %bh, %ah                                        #get 2nd byte
+    mov    $3, %rcx                                        #declare divisor
+    idiv   %rcx
+    lea    format1(%rip), %rdi
+    mov    %edx, %esi
+    call   printf
 .cont1:
+    #part 3
     pop    %rbx
     ret
